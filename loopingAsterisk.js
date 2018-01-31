6659094e374a55782a345cbd1846a30a513e0b40
JoviAndrew <jovian_andrew@hotmail.com>
2018-01-31 20:38:45 +0700

    hapus fungsi di nomor 2 dan 3

--- loopingAsterisk.js
+++ loopingAsterisk.js
@@ -11,17 +11,12 @@ for(row=5; row>0; row--){
 //====================================================================================
 //NOMOR 2
 
-//Function Adding Star
-function addStar(starTotal){
-    return starTotal + '*';
-}
-
 //Initial Star
 let currentStar='';
 
 for(row=5; row>0; row--){
     for(column=0; column<5; column++){
-        currentStar = addStar(currentStar);
+        currentStar = currentStar + '*';
     }  
     console.log(currentStar);
     currentStar=''; //Reset the number of Stars
@@ -30,16 +25,12 @@ for(row=5; row>0; row--){
 //====================================================================================
 //NOMOR 3
 
-function addStar(starTotal){
-    return starTotal + '*';
-}
-
 //Initial Star
 let currentStar='';
 
 for(row=1; row<=5; row++){
     for(column=0; column<row; column++){
-        currentStar = addStar(currentStar);
+        currentStar = currentStar + '*';
     }  
     console.log(currentStar);
     currentStar=''; //Reset the number of Stars
